c822bc8e83e26a5c4cf1f29ef08f7c838f32eaf2
LuanHMA <65981953+LuanHMA@users.noreply.github.com>
2021-08-31 22:41:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ let dataBase = {};
 
 function initCount(){
     let fieldDate = document.querySelector('#data')
-    
+    let date = new Date();
     let year = fieldDate.value.split('-')[0];
     let month = fieldDate.value.split('-')[1];
     let day = fieldDate.value.split('-')[2];
 
-    dataBase.userDay = day;
-    dataBase.userMonth = month;
-    dataBase.userYear = year;
-    localStorage.setItem('dataBase', JSON.stringify(dataBase));
-    location = 'contador.html';
+    if(year < date.getFullYear() || year == '' || month ==' ' || day == ''){
+        alert('Preencha os campos corretamente!')
+    }
+    else{
+        dataBase.userDay = day;
+        dataBase.userMonth = month;
+        dataBase.userYear = year;
+        localStorage.setItem('dataBase', JSON.stringify(dataBase));
+        location = 'contador.html';
+    }
+    
+    
 }
 
